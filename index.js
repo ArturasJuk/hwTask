6c74c3214b1aabd9465c0f3f51939af07396436f
Artūras Juknevičius <arturasjuknevicius@outlook.com>
2020-01-16 21:51:36 +0200

    working

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const imgList = document.getElementById('img-list');
-  const imgContainer = document.getElementById("img-container");
-  const imgDescription = document.getElementById("img-description");
+  const imgList = document.getElementsByClassName('img-list')[0];
+  const imgHeader = document.getElementsByClassName('img-header')[0];
+  const imgHeaderTitle = imgHeader.getElementsByTagName('h3')[0];
+  const imgHeaderDimensions = imgHeader.getElementsByTagName('span')[0];
+  const imgBody = document.getElementsByClassName('img-body')[0];
+
+  // const handleClick = (event) => {
+  //   // imgBody.styles.background-image = `"url(${})"`;
+  //   console.log(event.target.id);
+  // }
+
+  // imgList.addEventListener('click', handleClick.bind());
 
   const fetchImages = () => {
     fetch('https://picsum.photos/v2/list')
@@ -17,18 +26,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const appendToDOM = (images) => {
     images.forEach(img => {
+      const {
+        id
+      } = img;
       const newNode = document.createElement('li');
-      newNode.addEventListener('click', showFullImg.bind(null, img))
-      newNode.innerHTML = `<img id="${img.id}" src="https://picsum.photos/id/${img.id}/200/200">`;
+      newNode.addEventListener('click', handleImgClick.bind(null, img));
+      newNode.innerHTML = `<img id="${id}" src="https://picsum.photos/id/${id}/200/200">`;
+
       imgList.appendChild(newNode);
     });
   }
 
-  const showFullImg = (img) => {
-    const { id, author, width, height, download_url } = img;
-    imgDescription.innerHTML = `<span class="author">${author}</span><span class="dimensions">${width} x ${height}</span>`;
-    console.log(imgDescription.innerHTML);
-    imgContainer.style.backgroundImage = `url("${download_url}")`
+  const handleImgClick = (img) => {
+    const {
+      id,
+      author,
+      width,
+      height,
+      download_url
+    } = img;
+    imgHeaderTitle.innerHTML = `${author}`;
+    imgHeaderDimensions.innerHTML = `${width} x ${height}`;
+    imgBody.style.backgroundImage = `url("${download_url}")`;
   }
 
   fetchImages();
